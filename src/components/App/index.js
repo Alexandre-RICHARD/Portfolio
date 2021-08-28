f5a8de2c19dd6a6ad16c577215a4d5db89216180
Alexandre-RICHARD <alexandrichard99@gmail.com>
2021-08-28 12:19:41 +0200

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+function App() {
+  return (
+    <div className="app">
+      En route pour ce nouveau projet
+    </div>
+  );
+}
+
+export default App;
